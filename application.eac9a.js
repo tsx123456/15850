6bd7956a66ddf517f5b58151aec5cb5c83fa136c
上帝的梦 <1597691532@qq.com>
2025-09-11 18:29:42 +0800

    1

--- application.eac9a.js
+++ application.eac9a.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.f1952.json';
+          this.settingsPath = 'src/settings.7aa31.json';
           this.showFPS = false;
         }
 
--- index.7a137.js
+++ index.7a137.js
@@ -1,4 +1,4 @@
-System.register(["./application.4476b.js"], function (_export, _context) {
+System.register(["./application.eac9a.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.4476b.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application4476bJs) {
-      Application = _application4476bJs.Application;
+    setters: [function (_applicationEac9aJs) {
+      Application = _applicationEac9aJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
