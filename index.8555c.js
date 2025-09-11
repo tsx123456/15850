f1d7e96c5c976c6ee5eb388d510fb955019afe9d
上帝的梦 <1597691532@qq.com>
2025-09-11 18:18:28 +0800

    1

--- index.8555c.js
+++ index.8555c.js
@@ -1,4 +1,4 @@
-System.register(["./application.4fa30.js"], function (_export, _context) {
+System.register(["./application.4476b.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.4fa30.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application4fa30Js) {
-      Application = _application4fa30Js.Application;
+    setters: [function (_application4476bJs) {
+      Application = _application4476bJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
